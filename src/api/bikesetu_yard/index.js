a1308c7d50de2505341ffb9834f67b5fe7a12244
Jalay Movaliya <jalaym825@gmail.com>
2024-09-06 00:14:21 +0530

    fuck 9

--- src/api/bikesetu_yard/index.js
+++ src/api/bikesetu_yard/index.js
@@ -2,10 +2,11 @@ const updateStatus = require('./update-bike-status-yard');
 const getBikes = require('./get-bikes');
 
 const { Router } = require('express');
+const { verifyJWT } = require('../../utils/Middleware');
 
 const router = Router();
 
-router.post('/update-status/:id', updateStatus);
-router.get('/bikes', getBikes);
+router.post('/update-status/:id', verifyJWT, updateStatus);
+router.get('/bikes', verifyJWT, getBikes);
 
 module.exports = router;
