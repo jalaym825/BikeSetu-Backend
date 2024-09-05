19c3fa3605e58b991d105f56025d7bce2d58a768
Jalay Movaliya <jalaym825@gmail.com>
2024-09-05 23:13:29 +0530

    fuck 8

--- src/api/manufacturer/get-bikes.js
+++ src/api/manufacturer/get-bikes.js
@@ -4,10 +4,6 @@ module.exports = async function(req, res)  {
     try {
         const { status } = req.query;
 
-        if(!manufacturerId) {
-            return res.status(404).json({ error: 'Manufacturer not found' });
-        }
-
         const bikes = await prisma.bike.findMany({
             where: {
                 manufacturerId: req.user.sys_id,
